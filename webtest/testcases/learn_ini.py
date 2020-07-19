fa3ac77c7ea0bfab77b5678cdaf63e56827b0dc4
mmyhhhh <575864955@qq.com>
2020-07-19 10:36:07 +0800

    重新上传

--- webtest/testcases/learn_ini.py
+++
@@ -1,7 +0,0 @@
-from configparser import ConfigParser
-
-ccf=ConfigParser()
-ccf.read(r'C:\Users\Administrator\PycharmProjects\learn_pytest\webtest\testcases\learn.ini',encoding='utf-8')
-value=ccf['section']['markers']
-# value=eval[value]
-print(value,type(value))
